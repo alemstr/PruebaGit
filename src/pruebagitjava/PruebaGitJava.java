55efb83e1a40b2c935530927e7c48c5d5d80a0df
alberto benitez <albertobenitezn@iessansebastian.com>
2020-01-29 16:12:23 +0100

    primer commit de alberto desde la rama alberto

--- src/pruebagitjava/PruebaGitJava.java
+++ src/pruebagitjava/PruebaGitJava.java
@@ -16,6 +16,8 @@ public class PruebaGitJava {
      */
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        
+        System.out.println("desde alberto");
     }
     
 }
